42d33544d57cd255f7046e60c72eb58fe14b8a7a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-25 02:08:22 +0000

    before starting PA2

---
+++ nemu/src/cpu/exec/function/call-template.h
@@ -0,0 +1,22 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+
+make_helper(concat(call_i_v, SUFFIX)) {
+	int len =concat(call_i_v,SUFFIX)(cpu.eip+1);
+    reg_l( R_ESP) -= DATA_BYTE;
+
+    swaddr_write(reg_l(R_ESP),4 ,cpu.eip + len +1);
+
+    DATA_TYPE_S imm = op_src -> val;
+    print_asm("call\t%x",cpu.eip+1+len+imm);
+
+    cpu.eip += imm;
+    return len+1;
+
+}
+
+
+
+#include "cpu/exec/template-end.h"
